907e8976f26ee4322032b93c46295becea2d3cb0
Brett Shenk <brett.shenk5@gmail.com>
2023-06-29 08:35:23 -0400

    Update functions.php
    update comments

--- fixmycomputer/functions.php
+++ fixmycomputer/functions.php
@@ -3,15 +3,15 @@
 	File Include for all php files in the directory:   /includes/functions/
 
 		File					- Description
-		acf						- ACF related functions
+		acf					- ACF related functions
 		activation				- First time theme activation / Generate Theme Image
-		browser-upgrade			- IE browser warning
-		clean-up-wordpress		- SlimFast for WordPress. Also helps customize the admin bars for the client
+		browser-upgrade				- IE browser warning
+		clean-up-wordpress			- SlimFast for WordPress. Also helps customize the admin bars for the client
 		comments				- Disables comments
-		core-functions			- Nav walker class + body classes
+		core-functions				- Nav walker class + body classes
 		cpts					- All custom post types + taxonomies
-		custom-functions		- Random theme functions
-		enqueue-scripts			- All css and js resources are loaded here
+		custom-functions			- Random theme functions
+		enqueue-scripts				- All css and js resources are loaded here
 		favicon					- The favicon
 		image-size				- All image sizes
 		setup					- General WordPress setup settings
